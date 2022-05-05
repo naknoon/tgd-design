8e3fe76d2e4b0d9f4c069809ad772b2ba1160031
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-05-05 19:54:55 +0900

    Update PC.js

--- ins/PC.js
+++ ins/PC.js
@@ -1,6 +1,6 @@
 //TEST
 
-document.body.onload = addElement;
+/* document.body.onload = addElement;
 
 
 
@@ -23,3 +23,19 @@ function addElement () {
 
 
 };
+
+*/
+
+// create a new div element
+const newDiv = document.createElement("div");
+
+// and give it some content
+const newContent = document.createTextNode("환영합니다!");
+
+// add the text node to the newly created div
+newDiv.appendChild(newContent);
+
+
+// add the newly created element and its content into the DOM
+const currentDiv = document.querySelector('header')
+document.body.insertBefore(newDiv, currentDiv);
