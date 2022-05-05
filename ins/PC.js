b16ed8377c3c7804b6c85a4aa65519a90d00520e
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-05-05 20:24:23 +0900

    Update PC.js

--- ins/PC.js
+++ ins/PC.js
@@ -1,46 +1,29 @@
 //TEST
 
-/* document.body.onload = addElement;
+/* create a new div element
+const newDiv = document.createElement( 'div' );
+const newContent = document.createTextNode( "환영합니다!" );
+newDiv.appendChild(newContent);
 
-
-
-
-function addElement () {
-
-  // create a new div element
-  const newDiv = document.createElement("div");
-
-  // and give it some content
-  const newContent = document.createTextNode("환영합니다!");
-
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);
-
-
-  // add the newly created element and its content into the DOM
-  const currentDiv = document.querySelector('header')
-  document.body.insertBefore(newDiv, currentDiv);
-
-
-};
+document.body.prepend(newDiv)
 
 */
 
-// create a new div element
-const newDiv = document.createElement("div");
+let tag = 'div'
+let content = 'hello world!'
 
-// and give it some content
-const newContent = document.createTextNode("환영합니다!");
+function addElement () {
+    const addTag = document.createElement( tag );
+    const addContent = document.createTextNode( content );
+    addTag.appendChild(addContent);
+}
 
-// add the text node to the newly created div
-newDiv.appendChild(newContent);
+tag = 'p'
+content = '환영합니다! 앞입니다.'
+addElement()
+document.body.prepend(addTag)
 
-
-// add the newly created element and its content into the DOM
-document.body.prepend(newDiv)
-
-//const currentDiv = document.querySelector('header')
-//document.body.insertBefore(newDiv, currentDiv);
-
-
-
+tag = 'div'
+content = '환영합니다! 뒤입니다.'
+addElement()
+document.body.appendChild(addTag)
