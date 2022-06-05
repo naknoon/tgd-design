a4a51c2ea11b332b1bfc63e8afbd80f8e90b7db4
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-06-05 19:33:19 +0900

    Update PC Test.js

--- ins/PC Test.js
+++ ins/PC Test.js
@@ -14,12 +14,17 @@ setTimeout(() => {
         return target
     }
     
+    //내부 : prepend[앞] append[뒤]
+    //외부 : before[앞] after[뒤]
 
     //테스트 요소
     sel('body').append(ins('#test2'));
 
     //디스코드 버튼
     sel('footer').after(ins('#disBtn'));
+
+    //상단 공지바
+    sel('header').before(ins('#topNoti'));
 
 
 
