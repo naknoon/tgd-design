47cb623af3a2e9d590cb9ee77db18c624b4af1fe
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-06-05 13:39:05 +0900

    Update PC Beta.js

--- ins/PC Beta.js
+++ ins/PC Beta.js
@@ -1,5 +1,5 @@
 const footer = document.querySelector("footer");
-footer.insertAdjacentHTML('afterend', '<p><a href="https://tgd.kr/naknoon" target="_blank"><img class="discordBtn" src="https://cdn.discordapp.com/attachments/936953394415935568/982679557901537280/Naknoon.png" alt="Support Discord"></img></a></p>');
+footer.insertAdjacentHTML('afterend', '<p id="disButton"><a href="https://tgd.kr/naknoon" target="_blank"><img class="disBtnImg" src="https://cdn.discordapp.com/attachments/936953394415935568/982679557901537280/Naknoon.png" alt="Support Discord"></img></a></p>');
 
 
 
