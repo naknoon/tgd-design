273ca8909679bb387c56a066652d1dc8bebe78b6
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-06-05 13:24:08 +0900

    Update PC Beta.js

--- ins/PC Beta.js
+++ ins/PC Beta.js
@@ -1,2 +1,2 @@
 const footer = document.querySelector("footer");
-footer.insertAdjacentHTML('afterend', '<p><a href="https://discord.gg/RJ633SPb9z" target="_blank">Discord</a></p>');
+footer.insertAdjacentHTML('afterend', '<p><a href="tgd.kr/naknoon" target="_blank">Discord</a></p>');
