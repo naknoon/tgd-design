946d39d7a36198ddb72a2faf82019e22267d9c15
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-06-05 14:50:41 +0900

    Update PC Test.js

--- ins/PC Test.js
+++ ins/PC Test.js
@@ -1,14 +1,16 @@
-function ins(selector) {
-    let template = document.querySelector(selector);
-    let newContent = document.importNode(template, true);
-
-    return newContent
-}
-
-function sel(selector) {
-    let target = document.querySelector(selector);
+setTimeout(() => {
+    function ins(selector) {
+        let template = document.querySelector(selector);
+        let newContent = document.importNode(template, true);
     
-    return target
-}
-
-sel('body').appendChild(ins('#test2'));
+        return newContent
+    }
+    
+    function sel(selector) {
+        let target = document.querySelector(selector);
+        
+        return target
+    }
+    
+    sel('body').appendChild(ins('#test2'));
+}, 6000);
