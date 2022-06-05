41d76ac44dc6e930e59164388360a2a13f47dace
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-06-05 14:39:25 +0900

    Create PC Test.js

---
+++ ins/PC Test.js
@@ -0,0 +1,14 @@
+function ins(selector) {
+    let template = document.querySelector(selector);
+    let newContent = document.importNode(template, true);
+
+    return newContent
+}
+
+function sel(selector) {
+    let target = document.querySelector(selector);
+    
+    return target
+}
+
+sel('body').appendChild(ins('#test2'));
