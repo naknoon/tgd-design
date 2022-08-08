79e61e96920d0718c019106be07b878dfc0268bc
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-08-08 20:13:54 +0900

    테스트

--- ins/includeHTML.js
+++ ins/includeHTML.js
@@ -1,17 +1,44 @@
+//요소 삽입
+function ins(selector) {
+    let template = document.querySelector(selector);
+    let newContent = document.importNode(template, true);
+
+    return newContent;
+}
+
+function sel(selector) {
+    let target = document.querySelector(selector);
+
+    return target;
+}
+
 //Powered by https://kyung-a.tistory.com/18
-window.addEventListener('load', function() {
-    var allElements = document.getElementsByTagName('*');
-    Array.prototype.forEach.call(allElements, function(el) {
+window.addEventListener("load", function () {
+    var allElements = document.getElementsByTagName("*");
+    Array.prototype.forEach.call(allElements, function (el) {
         var includePath = el.dataset.includePath;
         if (includePath) {
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
                     el.outerHTML = this.responseText;
+
+                    //디스코드 버튼
+                    sel("footer").after(ins("#disBtn"));
+
+                    //상단 공지바
+                    sel("header").before(ins("#topNoti"));
+
+                    //유용한 링크
+                    sel("#content-area").prepend(ins("#pf-link"));
+
                 }
             };
-            xhttp.open('GET', includePath, true);
+            xhttp.open("GET", includePath, true);
             xhttp.send();
         }
     });
 });
+
+//내부 : prepend[앞] append[뒤]
+//외부 : before[앞] after[뒤]
