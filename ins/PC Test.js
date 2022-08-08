231354722beca1d0a6a7b96ad9ce3570dcad977b
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-08-08 18:06:30 +0900

    테스트

--- ins/PC Test.js
+++ ins/PC Test.js
@@ -1,36 +1,35 @@
-setTimeout(() => {
+//기본 설정
+function ins(selector) {
+    let template = document.querySelector(selector);
+    let newContent = document.importNode(template, true);
 
-    //기본 설정
-    function ins(selector) {
-        let template = document.querySelector(selector);
-        let newContent = document.importNode(template, true);
+    return newContent
+}
+
+function sel(selector) {
+    let target = document.querySelector(selector);
     
-        return newContent
-    }
-    
-    function sel(selector) {
-        let target = document.querySelector(selector);
-        
-        return target
-    }
-    
-    //내부 : prepend[앞] append[뒤]
-    //외부 : before[앞] after[뒤]
+    return target
+}
 
-    //테스트 요소
-    //sel('body').append(ins('#test2'));
+//내부 : prepend[앞] append[뒤]
+//외부 : before[앞] after[뒤]
 
-    //디스코드 버튼
-    sel('footer').after(ins('#disBtn'));
+//테스트 요소
+//sel('body').append(ins('#test2'));
 
-    //상단 공지바
-    sel('header').before(ins('#topNoti'));
+//디스코드 버튼
+sel('footer').after(ins('#disBtn'));
 
-    //유용한 링크
-    sel('#content-area').prepend(ins('#pf-link'))
+//상단 공지바
+sel('header').before(ins('#topNoti'));
+
+//유용한 링크
+sel('#content-area').prepend(ins('#pf-link'))
 
 
 
-}, 2000);
+//setTimeout(() => {
+//}, 2000);
 
 
