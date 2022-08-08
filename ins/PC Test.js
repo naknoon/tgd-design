8c51e46eb5f4f61f015ea0c67b9dda64bf8878e3
sonaknoon <53992907+sonaknoon@users.noreply.github.com>
2022-08-08 19:05:54 +0900

    제이쿼리 테스트

--- ins/PC Test.js
+++ ins/PC Test.js
@@ -1,31 +1,37 @@
 //기본 설정
-function ins(selector) {
-    let template = document.querySelector(selector);
-    let newContent = document.importNode(template, true);
 
-    return newContent
-}
-
-function sel(selector) {
-    let target = document.querySelector(selector);
+$(window).onload(function(){
+	function ins(selector) {
+        let template = document.querySelector(selector);
+        let newContent = document.importNode(template, true);
     
-    return target
-}
+        return newContent
+    }
+    
+    function sel(selector) {
+        let target = document.querySelector(selector);
+        
+        return target
+    }
+    
+    //내부 : prepend[앞] append[뒤]
+    //외부 : before[앞] after[뒤]
+    
+    //테스트 요소
+    //sel('body').append(ins('#test2'));
+    
+    //디스코드 버튼
+    sel('footer').after(ins('#disBtn'));
+    
+    //상단 공지바
+    sel('header').before(ins('#topNoti'));
+    
+    //유용한 링크
+    sel('#content-area').prepend(ins('#pf-link'))
+});
 
-//내부 : prepend[앞] append[뒤]
-//외부 : before[앞] after[뒤]
 
-//테스트 요소
-//sel('body').append(ins('#test2'));
 
-//디스코드 버튼
-sel('footer').after(ins('#disBtn'));
-
-//상단 공지바
-sel('header').before(ins('#topNoti'));
-
-//유용한 링크
-sel('#content-area').prepend(ins('#pf-link'))
 
 
 
